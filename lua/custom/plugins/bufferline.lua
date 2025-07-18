1eb9e28c53ea7caf39c4a7d1c03c364ac9b417c5
AtsuiOcha <oskadez@gmail.com>
2025-07-17 18:31:59 -0700

    bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,28 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('bufferline').setup {
+      options = {
+        mode = 'buffers', -- show buffer list, not tabpages
+        diagnostics = 'nvim_lsp',
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = 'slant',
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'File Explorer',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    }
+
+    -- Buffer navigation mappings (optional but highly recommended)
+    vim.keymap.set('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+    vim.keymap.set('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+  end,
+}
