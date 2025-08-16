551efa4655e11a206e62062094ff5950b2d9585c
Oskar Hernandez <oskadez@gmail.com>
2025-08-15 19:59:58 -0700

    typescript and claude stuff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -233,6 +233,7 @@ return {
             },
           },
         },
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -277,6 +278,7 @@ return {
         'basedpyright',
         'terraformls',
         'fixjson',
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
